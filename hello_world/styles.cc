8fd0c4da85d02c2c496a47dc6e99c12e2ab695c7
Rece Pellersels <89650163+rpellers@users.noreply.github.com>
2021-08-27 11:06:52 -0500

    Add files via upload
    html and css for hello world program

---
+++ hello_world/styles.cc
@@ -0,0 +1,14 @@
+.hello_world {
+    font-size: 48px;
+    text-align: left;
+    font-family: 'Tangerine', serif;
+    font-weight: 800;
+    font-style: Bold;
+    border: 2px solid black;
+    padding: 10px 20px 10px 20px;
+}
+.hello_world-box {
+    margin: 100px aut;
+    width: 360px;
+    align-content: left; 
+}
